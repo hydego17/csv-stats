94245a941eb4bcdf456fff73e5787ac8fb3b7433
Umma Ahimsha <uahimsha@gmail.com>
2020-12-02 19:19:00 +0700

    ...

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("test");
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
+
+const reader = new CsvFileReader("football.csv");
+reader.read();
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+// console.log({ manUnitedWins });
+console.log(reader.data[0]);
