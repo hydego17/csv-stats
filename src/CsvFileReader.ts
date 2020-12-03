6cb3173b25e0f42f1d0f84bac3f36430edf776ab
Umma Ahimsha <uahimsha@gmail.com>
2020-12-03 07:48:08 +0700

    added generic type for refactoring

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,13 +1,11 @@
 import fs from "fs";
-import { formatDate } from "./utils";
-import { MatchResult } from "./MatchResult";
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-export class CsvFileReader {
-  data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
@@ -18,18 +16,6 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(",");
       })
-      .map(
-        (row: string[]): MatchData => {
-          return [
-            formatDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult,
-            row[6],
-          ];
-        }
-      );
+      .map(this.mapRow);
   }
 }
